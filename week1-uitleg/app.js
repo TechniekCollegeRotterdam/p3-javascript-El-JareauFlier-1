de4e537a294e5d719725acb7c1f8e4e3670618c8
El-JareauFlier <9017738@student.zadkine.nl>
2023-02-02 09:45:41 +0100

    opdacht1-1

---
+++ week1-uitleg/app.js
@@ -0,0 +1,10 @@
+let text = Document.getElementById("change");
+
+
+text.style.color = "red";
+text.innerText = "<p>";
+text.style.fontWeight = "bold";
+
+
+// console.log(document.getElementById("change")
+// ); 
