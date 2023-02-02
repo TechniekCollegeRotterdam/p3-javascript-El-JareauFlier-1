3d7065059f69483bb60fd148dbf5c7a9f71107e9
Jason <83275145+jschaik@users.noreply.github.com>
2023-02-02 13:54:02 +0100

    Update app.js

--- week2/opdracht2-2/app.js
+++ week2/opdracht2-2/app.js
@@ -1,5 +1,7 @@
 let title = document.getElementById("title")
 
+ //in de opdracht staat: maak een functie genaamd kleur_aanpassen. Hoe kom jij dan aan "jouw_functie_hier"
+
 function jouw_functie_hier(){
 
     title.innerText = "Dit is een titel";
