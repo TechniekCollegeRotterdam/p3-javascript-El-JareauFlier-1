51d72e66a1974fb5630f35af4117fdcba08c7955
El-JareauFlier <9017738@student.zadkine.nl>
2023-02-09 13:20:13 +0100

    opdacht2-4

---
+++ week2/opdacht2herhaling/app.js
@@ -0,0 +1,27 @@
+// //variable for the title
+// let title = document.getElementById("title");
+// //variable for the image
+// let img = document.getElementById("image");
+// //variable for the button
+// let btn = document.getElementById("button");
+
+// img.style.width = "40%";
+
+// function removeimg() {
+//     title.style.color = "blue";
+//     img.style.display = "none";
+// }
+
+// function showimg() {
+//     title.style.color = "black";
+//     img.style.display = "block";
+
+// 
+
+function calc(num1, num2){
+    let answer = num1-num2;
+    console.log(answer);
+
+}
+
+calc(10 , 5)
---
+++ week2/opdracht2-4/app.js
@@ -0,0 +1,26 @@
+// oefening 1
+
+function calculate(num1, num2){
+    let answer = num1 + num2;
+    console.log("Het antwoord is" ,answer);
+}
+
+calculate(1 , 12)
+
+// oefening 2
+
+function multiplyByFive(num1){
+    let answer = num1 * 5;
+    console.log("Het antwoord is" , answer);
+}
+
+multiplyByFive(7)
+
+// oefening 3
+
+function calculateMinutes(num1){
+    let answer = num1 / 60;
+    console.log("Het antwoord is" , answer, "minuten");
+}
+
+calculateMinutes(3600)
