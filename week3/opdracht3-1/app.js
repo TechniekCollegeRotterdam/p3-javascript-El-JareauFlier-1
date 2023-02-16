314f777ceab92231cef461c432d2a60a5a24c771
El-JareauFlier <9017738@student.zadkine.nl>
2023-02-16 13:20:20 +0100

    opdracht3-1 done

---
+++ week3/opdracht3-1/app.js
@@ -0,0 +1,19 @@
+// variables
+let budget = 100
+let product = 60
+let ifelse = document.getElementById("ifelse")
+
+function canOrcant() {
+    if (product <= budget) {
+
+        ifelse.innerText = "U heeft genoeg geld!";
+        ifelse.style.color = "green"
+
+    } else {
+        ifelse.innerText = "Helaas, te weinig geldt";
+        ifelse.style.color = "red"
+
+    }
+}
+
+canOrcant()
