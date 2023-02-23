4c600f5e47abad214603efbd476135fa8f90db90
El-JareauFlier <9017738@student.zadkine.nl>
2023-02-23 11:47:02 +0100

    opdracht 4-2

--- week4/opdarcht4-1/app.js
+++ week4/opdarcht4-1/app.js
@@ -2,18 +2,37 @@ let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
 let answer = document.getElementById('answer');
 
+
 function multiply() {
-    answer.innerText = input1.value * input2.value;
+    if (input1.value > 0 || input2.value > 0) {
+        answer.innerText = input1.value * input2.value;
+    } else {
+        answer.innerText = "het getal is te laag"
+    }
 }
 
 function devide() {
-    answer.innerText = input1.value / input2.value;
+    if (input1.value > 0 || input2.value > 0) {
+        answer.innerText = input1.value / input2.value;
+    } else {
+        answer.innerText = "het getal is te laag"
+    }
 }
 
 function add() {
-    answer.innerText = Number(input1.value) + Number(input2.value);
+    if (input1.value > 0 || input2.value > 0) {
+
+        answer.innerText = Number(input1.value) + Number(input2.value);
+    } else {
+        answer.innerText = "het getal is te laag"
+    }
 }
 
 function minus() {
-    answer.innerText = input1.value - input2.value
+    if (input1.value > 0 || input2.value > 0) {
+
+        answer.innerText = input1.value - input2.value
+    } else {
+        answer.innerText = "het getal is te laag"
+    }
 }
