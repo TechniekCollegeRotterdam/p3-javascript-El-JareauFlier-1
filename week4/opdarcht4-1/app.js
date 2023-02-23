89b92fa10fae21a39342a58d9be5ccef1b5c5b25
El-JareauFlier <9017738@student.zadkine.nl>
2023-02-23 11:01:01 +0100

    opdracht 4-1
    finished

---
+++ week4/opdarcht4-1/app.js
@@ -0,0 +1,19 @@
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let answer = document.getElementById('answer');
+
+function multiply() {
+    answer.innerText = input1.value * input2.value;
+}
+
+function devide() {
+    answer.innerText = input1.value / input2.value;
+}
+
+function add() {
+    answer.innerText = Number(input1.value) + Number(input2.value);
+}
+
+function minus() {
+    answer.innerText = input1.value - input2.value
+}
