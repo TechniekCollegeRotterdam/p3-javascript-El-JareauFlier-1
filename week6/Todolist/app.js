8b7ba76b87ee13f23395deffadd939f5b6b16548
El-JareauFlier <91876956+El-JareauFlier@users.noreply.github.com>
2023-03-16 14:25:23 +0100

    6.2 to do list

--- week6/Todolist/app.js
+++ week6/Todolist/app.js
@@ -2,19 +2,30 @@ let inputField = document.getElementById("inputField");
 let button = document.getElementById("addToDo");
 let container = document.getElementById("toDoContainer");
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
 
     //maak een p element aan met createElement
-   let paragraph = document.createElement("p");
+    let paragraph = document.createElement("p");
 
-   //de waarde van de inputfield
-   paragraph.innerText = inputField.value;
+    //de waarde van de inputfield
+    paragraph.innerText = inputField.value;
 
-   //paragraph word toegevoegd aan de container
-   container.appendChild(paragraph);
+    //paragraph word toegevoegd aan de container
+    container.appendChild(paragraph);
 
     //input word dan leeggemaakt
-   inputField.value = '';
+    inputField.value = '';
+
+    paragraph.addEventListener("click", function () {
+        //als je er op klik maakt het een lijn aan je text
+        paragraph.style.textDecoration = "line-through"
+    })
+
+    paragraph.addEventListener("dblclick", function(){
+        //verwijderd de paragraph als je er dubbel op click
+        container.removeChild(paragraph)
+
+    })
+
 
 })
-
