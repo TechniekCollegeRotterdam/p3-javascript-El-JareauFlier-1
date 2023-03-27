28691da69e93c7746845665bb67a1b38d692438e
El-JareauFlier <91876956+El-JareauFlier@users.noreply.github.com>
2023-03-27 11:27:31 +0200

    opdracht7-3

---
+++ week7/opdracht7-3/app.js
@@ -0,0 +1,15 @@
+//opdracht1
+// for (let i = 0; i <= 100; i++){
+//     console.log(i)
+// }
+
+//opdracht2
+// for (let i = 100; i >= 0; i--){
+//     console.log(i);
+// }
+
+let shop = ['banana', 'appel', 'citroen'];
+
+for(let i = 1; i < shop.length; i++){
+console.log(shop[i]);
+}
